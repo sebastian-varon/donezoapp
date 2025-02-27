4330be151ed3f66a6300c358371951cca99833aa
sebastian-varon <sebasvaronmoreno@hotmail.com>
2025-02-26 21:06:29 -0500

    Initial Commit

---
+++ DONEZO/DONEZOApp.swift
@@ -0,0 +1,18 @@
+//
+//  DONEZOApp.swift
+//  DONEZO
+//
+//  Created by sebastian varon on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct DONEZOApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            LaunchScreenView()
+        }
+    }
+}
